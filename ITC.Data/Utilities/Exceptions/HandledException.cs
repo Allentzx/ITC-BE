1d942293efe441301b2a7f4f52ea5921a1a5d0f6
allentzx <allenwalker1789@gmail.com>
2022-12-20 14:48:27 +0700

    utilities commit

---
+++ ITC.Data/Utilities/Exceptions/HandledException.cs
@@ -0,0 +1,11 @@
+﻿namespace ITC.Data.Utilities.Exceptions;
+
+public class HandledException : Exception
+{
+    public int StatusCode { get; set; }
+
+    public HandledException(int statusCode, string message) : base(message)
+    {
+        StatusCode = statusCode;
+    }
+}
