e4639ac89a4f856ebbf703053eb451b54e6eec1f
allentzx <allenwalker1789@gmail.com>
2022-12-20 21:41:39 +0700

    commit

--- ITC.Data/Entities/Images.cs
+++ ITC.Data/Entities/Images.cs
@@ -1,17 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ITC.Data.Entities
 {
-	public class Role
-	{
+    public class Images
+    {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string NormalizationName { get; set; }
-        public string Description { get; set; }
-        public virtual ICollection<RoleUser> RoleUsers { get; set; }
+        public string? Url { get; set; }
     }
 }
-
